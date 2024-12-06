5f90ab15e412e956b1779d399d66bdcc1e0fc357
MinJeung-Kim <focso5@gmail.com>
2024-12-06 13:09:32 +0900

    Add: username validator

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/authcredential.dto';
@@ -14,6 +14,14 @@ export class UserRepository extends Repository<User> {
 
     const user = this.create({ username, password });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code == '23505') {
+        throw new ConflictException('Exixting username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
